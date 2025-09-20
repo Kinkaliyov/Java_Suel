14ee828b43974f7d18ef16a6e8b1f11272f9edaa
Kinkaliyov <suelgarabagli2@gmail.com>
2025-09-20 20:12:14 +0400

    Update Main.java

--- src/lesson5/Main.java
+++ src/lesson5/Main.java
@@ -2,13 +2,13 @@ package lesson5;
 
 public class Main {
     public static void main(String[] args) {
-        int theNumber = 100;
-        if(theNumber % 2 == 0) {
-            System.out.println("Число четное");
+        int theNumber = 0;
+        if (theNumber == 0){
+            System.out.println("Ошибка: Число не делится. Равняется нулю.");
         } else if(theNumber % 2 > 0) {
             System.out.println("Число нечетное");
-        } else {
-            System.out.println("Ошибка: Число не делится. Равняется нулю.");
+        } else if (theNumber % 2 == 0){
+            System.out.println("Число четное");
         }
     }
 }

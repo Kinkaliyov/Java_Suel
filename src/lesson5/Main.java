737a58238ee79710d22fd9dfaab6ef551e229d74
Kinkaliyov <suelgarabagli2@gmail.com>
2025-09-20 20:05:48 +0400

    Create Main.java

---
+++ src/lesson5/Main.java
@@ -0,0 +1,12 @@
+package lesson5;
+
+public class Main {
+    public static void main(String[] args) {
+        int theNumber = 100;
+        if(theNumber % 2 == 0) {
+            System.out.println("Число четное");
+        } else {
+            System.out.println("Число нечетное");
+        }
+    }
+}

d74d8bc5e470da135d9779ee00f5ff78509fe28d
Kinkaliyov <suelgarabagli2@gmail.com>
2025-10-01 20:52:45 +0400

    Create Classwork.java

---
+++ src/lesson7/Classwork.java
@@ -0,0 +1,11 @@
+package lesson7;
+
+public class Classwork {
+    public static void main(String[] args) {
+        for (int x = 0; x<21; x++) {
+            if (x % 3 == 0) {
+                System.out.println(x);
+            }
+        }
+    }
+}

8c394106a66a8fba2ed2d3b7d743ed404b8ce223
Kinkaliyov <suelgarabagli2@gmail.com>
2025-10-01 20:55:59 +0400

    Update Classwork.java

--- src/lesson7/Classwork.java
+++ src/lesson7/Classwork.java
@@ -2,8 +2,8 @@ package lesson7;
 
 public class Classwork {
     public static void main(String[] args) {
-        for (int x = 0; x<21; x++) {
-            if (x % 3 == 0) {
+        for (int x = 0; x < 21; x++) {
+            if (x % 3 != 0) {
                 System.out.println(x);
             }
         }

cff9a14de5627779fef648a41f0d4a41d0394248
Kinkaliyov <suelgarabagli2@gmail.com>
2025-10-01 20:58:01 +0400

    Update Classwork.java

--- src/lesson7/Classwork.java
+++ src/lesson7/Classwork.java
@@ -3,9 +3,10 @@ package lesson7;
 public class Classwork {
     public static void main(String[] args) {
         for (int x = 0; x < 21; x++) {
-            if (x % 3 != 0) {
-                System.out.println(x);
+            if (x % 3 == 0) {
+                continue;
             }
+            System.out.println(x);
         }
     }
 }
